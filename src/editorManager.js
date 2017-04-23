0c1c3b57862531a1961f1cd69313e78b6a1c1b64
Stephen Brady <stephen.brady@gmail.com>
2017-04-23 15:08:58 -0700

    SEE TODOS: Refactor EditorManager into a class. Fix references to this. No changes in logic.
    - Fix type annotations - methods for example shouldn't have the function type annotation

--- src/editorManager.js
+++ src/editorManager.js
@@ -5,11 +5,6 @@ import {stopPropagation, stopImmediatePropagation, isImmediatePropagationStopped
 import {getEditor} from './editors';
 import {eventManager as eventManagerObject} from './eventManager';
 
-export {EditorManager};
-
-// support for older versions of Handsontable
-Handsontable.EditorManager = EditorManager;
-
 function moveSelectionAfterEnter(selection, priv, shiftKey) {
   selection.setSelectedHeaders(false, false, false);
   var enterMoves = typeof priv.settings.enterMoves === 'function' ? priv.settings.enterMoves(event) : priv.settings.enterMoves;
@@ -69,223 +64,235 @@ function moveSelectionLeft(selection, shiftKey) {
   }
 }
 
-function EditorManager(instance, priv, selection) {
-  var _this = this,
-    destroyed = false,
-    eventManager,
-    activeEditor;
+class EditorManager {
 
-  eventManager = eventManagerObject(instance);
+  constructor(instance, priv, selection) {
+    this.instance = instance;
+    this.priv = priv;
+    this.selection = selection;
 
-  function onKeyDown(event) {
-    var ctrlDown, rangeModifier;
+    this.destroyed = false;
+    this.eventManager = eventManagerObject(instance);
+    this.activeEditor = null;
 
-    if (!instance.isListening()) {
-      return;
-    }
-    Handsontable.hooks.run(instance, 'beforeKeyDown', event);
+    this.init();
+  }
 
-    if (destroyed) {
-      return;
-    }
-    if (isImmediatePropagationStopped(event)) {
-      return;
-    }
-    priv.lastKeyCode = event.keyCode;
+  init() {
+    const onKeyDown = (event) => {
+      const instance = this.instance;
+      const selection = this.selection;
+      const activeEditor = this.activeEditor;
+      var ctrlDown, rangeModifier;
 
-    if (!selection.isSelected()) {
-      return;
-    }
-    // catch CTRL but not right ALT (which in some systems triggers ALT+CTRL)
-    ctrlDown = (event.ctrlKey || event.metaKey) && !event.altKey;
-
-    if (activeEditor && !activeEditor.isWaiting()) {
-      if (!isMetaKey(event.keyCode) && !isCtrlKey(event.keyCode) && !ctrlDown && !_this.isEditorOpened()) {
-        _this.openEditor('', event);
-
+      if (!instance.isListening()) {
         return;
       }
-    }
-    rangeModifier = event.shiftKey ? selection.setRangeEnd : selection.setRangeStart;
+      Handsontable.hooks.run(instance, 'beforeKeyDown', event);
 
-    switch (event.keyCode) {
+      if (this.destroyed) {
+        return;
+      }
+      if (isImmediatePropagationStopped(event)) {
+        return;
+      }
+      this.priv.lastKeyCode = event.keyCode;
 
-      case KEY_CODES.A:
-        if (!_this.isEditorOpened() && ctrlDown) {
-          selection.selectAll();
+      if (!selection.isSelected()) {
+        return;
+      }
+      // catch CTRL but not right ALT (which in some systems triggers ALT+CTRL)
+      ctrlDown = (event.ctrlKey || event.metaKey) && !event.altKey;
+
+      if (activeEditor && !activeEditor.isWaiting()) {
+        if (!isMetaKey(event.keyCode) && !isCtrlKey(event.keyCode) && !ctrlDown && !this.isEditorOpened()) {
+          this.openEditor('', event);
+
+          return;
+        }
+      }
+      rangeModifier = event.shiftKey ? selection.setRangeEnd : selection.setRangeStart;
+
+      switch (event.keyCode) {
+
+        case KEY_CODES.A:
+          if (!this.isEditorOpened() && ctrlDown) {
+            selection.selectAll();
+
+            event.preventDefault();
+            stopPropagation(event);
+          }
+          break;
+
+        case KEY_CODES.ARROW_UP:
+          if (this.isEditorOpened() && !activeEditor.isWaiting()) {
+            this.closeEditorAndSaveChanges(ctrlDown);
+          }
+          moveSelectionUp(selection, event.shiftKey);
 
           event.preventDefault();
           stopPropagation(event);
-        }
-        break;
+          break;
 
-      case KEY_CODES.ARROW_UP:
-        if (_this.isEditorOpened() && !activeEditor.isWaiting()) {
-          _this.closeEditorAndSaveChanges(ctrlDown);
-        }
-        moveSelectionUp(selection, event.shiftKey);
-
-        event.preventDefault();
-        stopPropagation(event);
-        break;
-
-      case KEY_CODES.ARROW_DOWN:
-        if (_this.isEditorOpened() && !activeEditor.isWaiting()) {
-          _this.closeEditorAndSaveChanges(ctrlDown);
-        }
-
-        moveSelectionDown(selection, event.shiftKey);
-
-        event.preventDefault();
-        stopPropagation(event);
-        break;
-
-      case KEY_CODES.ARROW_RIGHT:
-        if (_this.isEditorOpened() && !activeEditor.isWaiting()) {
-          _this.closeEditorAndSaveChanges(ctrlDown);
-        }
-
-        moveSelectionRight(selection, event.shiftKey);
-
-        event.preventDefault();
-        stopPropagation(event);
-        break;
-
-      case KEY_CODES.ARROW_LEFT:
-        if (_this.isEditorOpened() && !activeEditor.isWaiting()) {
-          _this.closeEditorAndSaveChanges(ctrlDown);
-        }
-
-        moveSelectionLeft(selection, event.shiftKey);
-
-        event.preventDefault();
-        stopPropagation(event);
-        break;
-
-      case KEY_CODES.TAB:
-        selection.setSelectedHeaders(false, false, false);
-        var tabMoves = typeof priv.settings.tabMoves === 'function' ? priv.settings.tabMoves(event) : priv.settings.tabMoves;
-
-        if (event.shiftKey) {
-          // move selection left
-          selection.transformStart(-tabMoves.row, -tabMoves.col);
-        } else {
-          // move selection right (add a new column if needed)
-          selection.transformStart(tabMoves.row, tabMoves.col, true);
-        }
-        event.preventDefault();
-        stopPropagation(event);
-        break;
-
-      case KEY_CODES.BACKSPACE:
-      case KEY_CODES.DELETE:
-        selection.empty(event);
-        _this.prepareEditor();
-        event.preventDefault();
-        break;
-
-      case KEY_CODES.F2:
-        /* F2 */
-        _this.openEditor(null, event);
-
-        if (activeEditor) {
-          activeEditor.enableFullEditMode();
-        }
-        event.preventDefault(); // prevent Opera from opening 'Go to Page dialog'
-        break;
-
-      case KEY_CODES.ENTER:
-        /* return/enter */
-        if (_this.isEditorOpened()) {
-
-          if (activeEditor && activeEditor.state !== Handsontable.EditorState.WAITING) {
-            _this.closeEditorAndSaveChanges(ctrlDown);
+        case KEY_CODES.ARROW_DOWN:
+          if (this.isEditorOpened() && !activeEditor.isWaiting()) {
+            this.closeEditorAndSaveChanges(ctrlDown);
           }
-          moveSelectionAfterEnter(selection, priv, event.shiftKey);
 
-        } else {
-          if (instance.getSettings().enterBeginsEditing) {
-            _this.openEditor(null, event);
+          moveSelectionDown(selection, event.shiftKey);
 
-            if (activeEditor) {
-              activeEditor.enableFullEditMode();
-            }
+          event.preventDefault();
+          stopPropagation(event);
+          break;
+
+        case KEY_CODES.ARROW_RIGHT:
+          if (this.isEditorOpened() && !activeEditor.isWaiting()) {
+            this.closeEditorAndSaveChanges(ctrlDown);
+          }
+
+          moveSelectionRight(selection, event.shiftKey);
+
+          event.preventDefault();
+          stopPropagation(event);
+          break;
+
+        case KEY_CODES.ARROW_LEFT:
+          if (this.isEditorOpened() && !activeEditor.isWaiting()) {
+            this.closeEditorAndSaveChanges(ctrlDown);
+          }
+
+          moveSelectionLeft(selection, event.shiftKey);
+
+          event.preventDefault();
+          stopPropagation(event);
+          break;
+
+        case KEY_CODES.TAB:
+          selection.setSelectedHeaders(false, false, false);
+          var tabMoves = typeof this.priv.settings.tabMoves === 'function' ? this.priv.settings.tabMoves(event) : this.priv.settings.tabMoves;
+
+          if (event.shiftKey) {
+            // move selection left
+            selection.transformStart(-tabMoves.row, -tabMoves.col);
           } else {
-            moveSelectionAfterEnter(selection, priv, event.shiftKey);
+            // move selection right (add a new column if needed)
+            selection.transformStart(tabMoves.row, tabMoves.col, true);
           }
-        }
-        event.preventDefault(); // don't add newline to field
-        stopImmediatePropagation(event); // required by HandsontableEditor
-        break;
+          event.preventDefault();
+          stopPropagation(event);
+          break;
 
-      case KEY_CODES.ESCAPE:
-        if (_this.isEditorOpened()) {
-          _this.closeEditorAndRestoreOriginalValue(ctrlDown);
-        }
-        event.preventDefault();
-        break;
+        case KEY_CODES.BACKSPACE:
+        case KEY_CODES.DELETE:
+          selection.empty(event);
+          this.prepareEditor();
+          event.preventDefault();
+          break;
 
-      case KEY_CODES.HOME:
-        selection.setSelectedHeaders(false, false, false);
-        if (event.ctrlKey || event.metaKey) {
-          rangeModifier(new WalkontableCellCoords(0, priv.selRange.from.col));
-        } else {
-          rangeModifier(new WalkontableCellCoords(priv.selRange.from.row, 0));
-        }
-        event.preventDefault(); // don't scroll the window
-        stopPropagation(event);
-        break;
+        case KEY_CODES.F2:
+          /* F2 */
+          this.openEditor(null, event);
 
-      case KEY_CODES.END:
-        selection.setSelectedHeaders(false, false, false);
-        if (event.ctrlKey || event.metaKey) {
-          rangeModifier(new WalkontableCellCoords(instance.countRows() - 1, priv.selRange.from.col));
-        } else {
-          rangeModifier(new WalkontableCellCoords(priv.selRange.from.row, instance.countCols() - 1));
-        }
-        event.preventDefault(); // don't scroll the window
-        stopPropagation(event);
-        break;
+          if (activeEditor) {
+            activeEditor.enableFullEditMode();
+          }
+          event.preventDefault(); // prevent Opera from opening 'Go to Page dialog'
+          break;
 
-      case KEY_CODES.PAGE_UP:
-        selection.setSelectedHeaders(false, false, false);
-        selection.transformStart(-instance.countVisibleRows(), 0);
-        event.preventDefault(); // don't page up the window
-        stopPropagation(event);
-        break;
+        case KEY_CODES.ENTER:
+          /* return/enter */
+          if (this.isEditorOpened()) {
 
-      case KEY_CODES.PAGE_DOWN:
-        selection.setSelectedHeaders(false, false, false);
-        selection.transformStart(instance.countVisibleRows(), 0);
-        event.preventDefault(); // don't page down the window
-        stopPropagation(event);
-        break;
-    }
-  }
+            if (activeEditor && activeEditor.state !== Handsontable.EditorState.WAITING) {
+              this.closeEditorAndSaveChanges(ctrlDown);
+            }
+            moveSelectionAfterEnter(selection, this.priv, event.shiftKey);
 
-  function init() {
-    instance.addHook('afterDocumentKeyDown', onKeyDown);
+          } else {
+            if (instance.getSettings().enterBeginsEditing) {
+              this.openEditor(null, event);
 
-    eventManager.addEventListener(document.documentElement, 'keydown', function(event) {
-      if (!destroyed) {
-        instance.runHooks('afterDocumentKeyDown', event);
+              if (activeEditor) {
+                activeEditor.enableFullEditMode();
+              }
+            } else {
+              moveSelectionAfterEnter(selection, this.priv, event.shiftKey);
+            }
+          }
+          event.preventDefault(); // don't add newline to field
+          stopImmediatePropagation(event); // required by HandsontableEditor
+          break;
+
+        case KEY_CODES.ESCAPE:
+          if (this.isEditorOpened()) {
+            this.closeEditorAndRestoreOriginalValue(ctrlDown);
+          }
+          event.preventDefault();
+          break;
+
+        case KEY_CODES.HOME:
+          selection.setSelectedHeaders(false, false, false);
+          if (event.ctrlKey || event.metaKey) {
+            rangeModifier(new WalkontableCellCoords(0, this.priv.selRange.from.col));
+          } else {
+            rangeModifier(new WalkontableCellCoords(this.priv.selRange.from.row, 0));
+          }
+          event.preventDefault(); // don't scroll the window
+          stopPropagation(event);
+          break;
+
+        case KEY_CODES.END:
+          selection.setSelectedHeaders(false, false, false);
+          if (event.ctrlKey || event.metaKey) {
+            rangeModifier(new WalkontableCellCoords(instance.countRows() - 1, this.priv.selRange.from.col));
+          } else {
+            rangeModifier(new WalkontableCellCoords(this.priv.selRange.from.row, instance.countCols() - 1));
+          }
+          event.preventDefault(); // don't scroll the window
+          stopPropagation(event);
+          break;
+
+        case KEY_CODES.PAGE_UP:
+          selection.setSelectedHeaders(false, false, false);
+          selection.transformStart(-instance.countVisibleRows(), 0);
+          event.preventDefault(); // don't page up the window
+          stopPropagation(event);
+          break;
+
+        case KEY_CODES.PAGE_DOWN:
+          selection.setSelectedHeaders(false, false, false);
+          selection.transformStart(instance.countVisibleRows(), 0);
+          event.preventDefault(); // don't page down the window
+          stopPropagation(event);
+          break;
       }
-    });
+    };
 
-    function onDblClick(event, coords, elem) {
+    const keydownHandler = (event) => {
+      if (!this.destroyed) {
+        this.instance.runHooks('afterDocumentKeyDown', event);
+      }
+    };
+
+    const onDblClick = (event, coords, elem) => {
       // may be TD or TH
       if (elem.nodeName == 'TD') {
-        _this.openEditor();
+        this.openEditor();
 
-        if (activeEditor) {
-          activeEditor.enableFullEditMode();
+        if (this.activeEditor) {
+          this.activeEditor.enableFullEditMode();
         }
       }
-    }
-    instance.view.wt.update('onCellDblClick', onDblClick);
+    };
 
-    instance.addHook('afterDestroy', function() {
-      destroyed = true;
+    this.instance.addHook('afterDocumentKeyDown', onKeyDown);
+
+    this.eventManager.addEventListener(document.documentElement, 'keydown', keydownHandler);
+
+    this.instance.view.wt.update('onCellDblClick', onDblClick);
+
+    this.instance.addHook('afterDestroy', () => {
+      this.destroyed = true;
     });
   }
 
@@ -296,9 +303,9 @@ function EditorManager(instance, priv, selection) {
    * @memberof! Handsontable.EditorManager#
    * @param {Boolean} revertOriginal
    */
-  this.destroyEditor = function(revertOriginal) {
+  destroyEditor(revertOriginal) {
     this.closeEditor(revertOriginal);
-  };
+  }
 
   /**
    * Get active editor.
@@ -307,9 +314,9 @@ function EditorManager(instance, priv, selection) {
    * @memberof! Handsontable.EditorManager#
    * @returns {*}
    */
-  this.getActiveEditor = function() {
-    return activeEditor;
-  };
+  getActiveEditor() {
+    return this.activeEditor;
+  }
 
   /**
    * Prepare text input to be displayed at given grid cell.
@@ -317,18 +324,23 @@ function EditorManager(instance, priv, selection) {
    * @function prepareEditor
    * @memberof! Handsontable.EditorManager#
    */
-  this.prepareEditor = function() {
+  prepareEditor() {
     var row, col, prop, td, originalValue, cellProperties, editorClass;
 
+    const instance = this.instance;
+    const priv = this.priv;
+    let activeEditor = this.activeEditor;
+
     if (activeEditor && activeEditor.isWaiting()) {
-      this.closeEditor(false, false, function(dataSaved) {
+      this.closeEditor(false, false, (dataSaved) => {
         if (dataSaved) {
-          _this.prepareEditor();
+          this.prepareEditor();
         }
       });
 
       return;
     }
+
     row = priv.selRange.highlight.row;
     col = priv.selRange.highlight.col;
     prop = instance.colToProp(col);
@@ -341,11 +353,12 @@ function EditorManager(instance, priv, selection) {
     if (editorClass) {
       activeEditor = Handsontable.editors.getEditor(editorClass, instance);
       activeEditor.prepare(row, col, prop, td, originalValue, cellProperties);
-
     } else {
       activeEditor = void 0;
     }
-  };
+
+    this.activeEditor = activeEditor;
+  }
 
   /**
    * Check is editor is opened/showed.
@@ -354,9 +367,9 @@ function EditorManager(instance, priv, selection) {
    * @memberof! Handsontable.EditorManager#
    * @returns {Boolean}
    */
-  this.isEditorOpened = function() {
-    return activeEditor && activeEditor.isOpened();
-  };
+  isEditorOpened() {
+    return this.activeEditor && this.activeEditor.isOpened();
+  }
 
   /**
    * Open editor with initial value.
@@ -366,17 +379,18 @@ function EditorManager(instance, priv, selection) {
    * @param {String} initialValue
    * @param {DOMEvent} event
    */
-  this.openEditor = function(initialValue, event) {
+  openEditor(initialValue, event) {
+    const activeEditor = this.activeEditor;
     if (activeEditor && !activeEditor.cellProperties.readOnly) {
       activeEditor.beginEditing(initialValue, event);
     } else if (activeEditor && activeEditor.cellProperties.readOnly) {
 
       // move the selection after opening the editor with ENTER key
       if (event && event.keyCode === KEY_CODES.ENTER) {
-        moveSelectionAfterEnter(selection, priv, false);
+        moveSelectionAfterEnter(this.selection, this.priv, false);
       }
     }
-  };
+  }
 
   /**
    * Close editor, finish editing cell.
@@ -387,15 +401,13 @@ function EditorManager(instance, priv, selection) {
    * @param {Boolean} [ctrlDown]
    * @param {Function} [callback]
    */
-  this.closeEditor = function(restoreOriginalValue, ctrlDown, callback) {
-    if (activeEditor) {
-      activeEditor.finishEditing(restoreOriginalValue, ctrlDown, callback);
-    } else {
-      if (callback) {
-        callback(false);
-      }
+  closeEditor(restoreOriginalValue, ctrlDown, callback) {
+    if (this.activeEditor) {
+      this.activeEditor.finishEditing(restoreOriginalValue, ctrlDown, callback);
+    } else if (callback) {
+      callback(false);
     }
-  };
+  }
 
   /**
    * Close editor and save changes.
@@ -404,9 +416,9 @@ function EditorManager(instance, priv, selection) {
    * @memberof! Handsontable.EditorManager#
    * @param {Boolean} ctrlDown
    */
-  this.closeEditorAndSaveChanges = function(ctrlDown) {
+  closeEditorAndSaveChanges(ctrlDown) {
     return this.closeEditor(false, ctrlDown);
-  };
+  }
 
   /**
    * Close editor and restore original value.
@@ -415,9 +427,12 @@ function EditorManager(instance, priv, selection) {
    * @memberof! Handsontable.EditorManager#
    * @param {Boolean} ctrlDown
    */
-  this.closeEditorAndRestoreOriginalValue = function(ctrlDown) {
+  closeEditorAndRestoreOriginalValue(ctrlDown) {
     return this.closeEditor(true, ctrlDown);
-  };
-
-  init();
+  }
 }
+
+// support for older versions of Handsontable
+Handsontable.EditorManager = EditorManager;
+
+export {EditorManager};
